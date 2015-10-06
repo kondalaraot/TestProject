919cb16c55be6383e68b12dcb806ffb65b96f1fd
kondalaraot <kondal.rao58@gmail.com>
2015-10-06 13:20:03 +0530

    Reverse an array

--- src/com/practise/TestJavaProj.java
+++ src/com/practise/TestJavaProj.java
@@ -1,10 +1,24 @@
 package com.practise;
 
 public class TestJavaProj {
-
+	
+	static char[] name ={'K','O','N','D','A','L','R'};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(name);
+		reverse(name);
+		System.out.println(name);
 
+	}
+	
+	private static void reverse(char[] charArray) {
+		for(int i = 0; i < charArray.length / 2; i++)
+		{
+		    char temp = charArray[i];
+		    charArray[i] = charArray[charArray.length - i - 1];
+		    charArray[charArray.length - i - 1] = temp;
+		}
 	}
 
 }
